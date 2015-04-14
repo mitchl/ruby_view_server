5c3cbd977e6d92e9996f33a7fc76b85a08dc4bc4
mitchl <mitch.m.lee@gmail.com>
2015-04-13 21:21:53 -0700

    erb_example.rb initial modification

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,20 @@
 require 'erb'
+require 'date'
 
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+page_string = "
+<html>
+  <body>
+    <p>I am a web page</p>
+    <p>The time is now:
+    <%= Time.now %></p>
+  </body>
+</html>"
+
+template2 = ERB.new page_string
+puts template2.result(binding)
+
+
